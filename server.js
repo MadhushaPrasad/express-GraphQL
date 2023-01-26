5ed66c1fdacbad3751bbbe4ab93def30224e308e
MadhushaPrasad <madushaprasad21@gmail.com>
2023-01-26 13:53:24 +0530

    add comments

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// Description: This is the entry point of the application
+//import express from "express";
 const express = require("express");
 
+//import {graphqlHTTP} from "express-graphql";
 const {graphqlHTTP} = require("express-graphql");
 
+//assign port
 const port = 5000;
 
+//create express app
 const app = express();
 
+//create graphql endpoint
 app.use('/graphql', graphqlHTTP(
     {
       graphiql: true,
     }
 ))
 
-
+//listen to port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
