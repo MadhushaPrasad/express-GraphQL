e0a34e108df4c97233178fbe4ed9ab284ff2ed44
MadhushaPrasad <madushaprasad21@gmail.com>
2023-01-26 14:00:42 +0530

    create a schema

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 // Description: This is the entry point of the application
 //import express from "express";
 const express = require("express");
-
-//import {graphqlHTTP} from "express-graphql";
-const {graphqlHTTP} = require("express-graphql");
-
 //assign port
 const port = 5000;
+//import {graphqlHTTP} from "express-graphql";
+const {graphqlHTTP} = require("express-graphql");
+//import GraphQLSchema and GraphQLObjectType  from "graphql";
+const {GraphQLSchema, GraphQLObjectType} = require("graphql");
+
+//create schema
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({//query is the root query
+        name: 'Hello World',//name of the query
+        fields: () => ({//fields is the object that contains the data
+            message: {//message is the field
+                type: GraphQLString,//type of the field
+                resolve: () => 'Hello World'//resolve is the function that returns the data
+            }
+        })
+    })
+})
+
 
 //create express app
 const app = express();
@@ -14,11 +28,11 @@ const app = express();
 //create graphql endpoint
 app.use('/graphql', graphqlHTTP(
     {
-      graphiql: true,
+        graphiql: true,
     }
 ))
 
 //listen to port
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
