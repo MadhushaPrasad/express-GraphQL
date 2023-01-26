38625b52145e5284d9a2c9cb02d5d5be0c9f2dd1
MadhushaPrasad <madushaprasad21@gmail.com>
2023-01-26 14:14:37 +0530

    create data as authors and books

--- server.js
+++ server.js
@@ -6,7 +6,26 @@ const port = 5000;
 //import {graphqlHTTP} from "express-graphql";
 const {graphqlHTTP} = require("express-graphql");
 //import GraphQLSchema,GraphQLObjectType and GraphQLString  from "graphql";
-const {GraphQLSchema, GraphQLObjectType,GraphQLString} = require("graphql");
+const {GraphQLSchema, GraphQLObjectType, GraphQLString} = require("graphql");
+
+//create authors data
+const authors = [
+    { id: 1, name: 'J. K. Rowling' },
+    { id: 2, name: 'J. R. R. Tolkien' },
+    { id: 3, name: 'Brent Weeks' }
+]
+
+//create books data
+const books = [
+    {id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1},
+    {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
+    {id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1},
+    {id: 4, name: 'The Fellowship of the Ring', authorId: 2},
+    {id: 5, name: 'The Two Towers', authorId: 2},
+    {id: 6, name: 'The Return of the King', authorId: 2},
+    {id: 7, name: 'The Way of Shadows', authorId: 3},
+    {id: 8, name: 'Beyond the Shadows', authorId: 3}
+];
 
 //create schema
 const schema = new GraphQLSchema({
